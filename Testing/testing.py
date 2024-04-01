37ec0e094e2c46ab1d81ea99c3e7798a318feb74
mridul-g <58457291+mridul-g@users.noreply.github.com>
2024-04-01 19:35:34 +0530

    test added

--- Testing/testing.py
+++ Testing/testing.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+import datetime
 
 class Tests:
 
@@ -173,9 +175,97 @@ class Tests:
         self.logout()
         print("Authentication Tests Passed")
 
+    def complaints_student(self):
+        self.student_login()
+        self.driver.implicitly_wait(0.5)
+        self.driver.find_elements(By.CLASS_NAME, "guest-room-booking-text16")[0].click()
+        
+        
+
+        #clinking on new-complaint
+        self.driver.find_elements(By.CLASS_NAME, "btn.btn-success")[0].click()
+
+        #making requests
+
+        #request 1 : 
+        self.driver.find_elements(By.CLASS_NAME, "room_text")[1].send_keys("D-213")
+
+        #complaint sub-cateogry and category :
+        self.driver.find_element(By.ID, "dropdown1").send_keys("Electrical")
+        self.driver.find_element(By.ID, "dropdown2").send_keys("Fan not working")
+
+
+        self.driver.find_elements(By.CLASS_NAME, "comments_rec")[0].send_keys("Room check")
+        self.driver.find_elements(By.CLASS_NAME, "submit_rec")[0].click()
+        self.logout()
+
+        #admin_login
+        self.driver.implicitly_wait(0.5)
+        self.admin_login()
+        self.driver.explicitly_wait(5)
+        self.driver.find_elements(By.CLASS_NAME, "guest-room-booking-text16")[0].click()
+        assert(self.driver.find_elements(By.CLASS_NAME, "a")[0].text == "mridulg22")
+
+    def mark_resolved_complaint(self):
+        self.student_login()
+        self.driver.implicitly_wait(0.5)
+        self.driver.find_elements(By.CLASS_NAME, "guest-room-booking-text16")[0].click()
+
+        #mark_resolved
+        self.driver.find_elements(By.CLASS_NAME, "btn.btn-outline-success.btn-sm")[0].click()
+
+        #try to mark some-one else's complaint as resolved 
+        self.driver.find_elements(By.CLASS_NAME, "btn.btn-outline-success.btn-sm")[1].click()
+
+    def test1_new_annc(self,catch_item):
+         #Announcement being added correctly in the admin page
+        self.admin_login()
+        self.driver.find_element(By.NAME, "add_hidden_item").click()        
+        self.driver.find_element(By.NAME, catch_item).send_keys("Test Announcement")
+        self.driver.find_element(By.NAME, "submit").click() 
+        assert(self.driver.find_elements(By.CLASS_NAME, "alert-success")[0].text == "Changes made successfully.")
+        time.sleep(5)
+        self.logout()
+        #New Announcement visible to the user
+        self.student_login()
+        assert(self.driver.find_elements(By.TAG_NAME, "td")[4].text == "Test Announcement" and self.driver.find_elements(By.TAG_NAME, "td")[5].text ==datetime.date.today().strftime("%B %d, %Y"))
+        time.sleep(5)
+        self.logout()
+
+    def test2_edit_annc(self,catch_item):
+         #Announcement being edited correctly in the admin page
+        self.admin_login()
+        self.driver.find_element(By.NAME, "edit").click() 
+        self.driver.find_element(By.NAME, catch_item).clear()       
+        self.driver.find_element(By.NAME, catch_item).send_keys("Test Announcement Edited")
+        self.driver.find_element(By.NAME, "submit").click() 
+        assert(self.driver.find_elements(By.CLASS_NAME, "alert-success")[0].text == "Changes made successfully.")
+        time.sleep(5)
+        self.logout()
+        #Edited Announcement visible to the user
+        self.student_login()
+        assert(self.driver.find_elements(By.TAG_NAME, "td")[4].text == "Test Announcement Edited" and self.driver.find_elements(By.TAG_NAME, "td")[5].text ==datetime.date.today().strftime("%B %d, %Y"))
+        time.sleep(5)
+        self.logout()
+
+    def delete_test_annc(self):
+        self.admin_login()
+        self.driver.find_elements(By.TAG_NAME, "td")[5].click()
+        time.sleep(5)
+        self.logout()
+        
+        
+    def announcements_test(self):
+        annc_num=39
+        catch_item="item"+str(annc_num)
+        self.test1_new_annc(catch_item)
+        self.test2_edit_annc(catch_item)
+        self.delete_test_annc()
+
 
 if(__name__ == "__main__"):
     test = Tests()
     # test.dashboard_student()
     # test.dashboard_admin()
     test.authentication_tests()
+    test.complaints_student()
